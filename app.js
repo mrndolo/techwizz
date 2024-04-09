fcb8f04233f782ac416c6a9484d6bd25f99b8e2d
mrndolo <callistomutunga@gmail.com>
2024-04-09 16:03:33 +0300

    Added dark theme to the website

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ window.addEventListener('scroll', function(){
         scrolltp.style.opacity = 0;
     }
 })
+
+// Dark mode theme
+const themeToggle = document.querySelector(".checkbox");
+const body = document.querySelector('body');
+const darkmode = localStorage.getItem("dark");
+
+
+if(darkmode){
+    body.classList.add("dark");
+    themeToggle.checked = true;
+}
+
+themeToggle.addEventListener('change', function() {
+    body.classList.toggle("dark");
+
+    if(body.classList.contains('dark')){
+        localStorage.setItem("dark", "active");
+    }else {
+        localStorage.removeItem("dark")
+    }
+})
